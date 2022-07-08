6e94f5e7ba4a1f58a2f844bbd5eaa73d0a3f5d93
cloudsony999 <cloudsony999@gmail.com>
2022-07-08 19:28:57 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Login } from './login';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'templateform';
+  model: Login;
+  constructor()
+  {
+    this.model=new Login('a@gmail.com','1234',true);
+  }
+  onSubmit(loginform: { value: any; status: any; })
+  {
+    console.log(loginform.value);
+    console.log(loginform.status);
+      
+  }
+
+  newLogin()
+  {
+    this.model=new Login("","",false);
+  }
 }
---
+++ src/app/login.ts
@@ -0,0 +1,13 @@
+export class Login {
+    constructor(
+        public email: string,
+        public password:string,
+        public rememberpassword?:boolean
+    )
+    {
+        console.log("I am cons....");
+
+    }
+
+    
+}
